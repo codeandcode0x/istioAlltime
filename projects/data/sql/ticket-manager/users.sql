54092b06dc1bc5fb4691346f4ce71703d4dd6b0e
ethanluo <luocheng1@coding.net>
2021-05-30 17:38:57 +0800

    update dockerfile

--- projects/data/sql/ticket-manager/users.sql
+++ projects/data/sql/ticket-manager/users.sql
@@ -4,4 +4,4 @@ LIMIT 0, 1000
 
 -- Date: 2021-05-24 23:49
 */
-INSERT INTO ticket.users(id,name,password,email,age,birthday,member_number,created_at,updated_at,deleted_at,role) VALUES (1,'itcast','$2a$10$E3H2UH9ZZhIjFQ7Rl5fTzO9S8B657./80XsKwOUtolwXoYNqCnESi','82713@itcast.cn',0,'2021-05-24 23:39:01.278','1','2021-05-24 23:39:01.280','2021-05-24 23:39:01.280',NULL,'管理员');
+INSERT INTO ticket.users(name,password,email,age,birthday,member_number,created_at,updated_at,deleted_at,role) VALUES ('itcast','$2a$10$E3H2UH9ZZhIjFQ7Rl5fTzO9S8B657./80XsKwOUtolwXoYNqCnESi','82713@itcast.cn',0,'2021-05-24 23:39:01.278','1','2021-05-24 23:39:01.280','2021-05-24 23:39:01.280',NULL,'管理员');
